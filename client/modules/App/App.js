838d7c83a32d24bd5bb21ae9d03f7681ddc6df0d
Mitchell <mitchlee@amazon.com>
2016-08-16 23:22:14 -0500

    Integrating Bootstrap

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import styles from './App.css';
 
 // Import Components
+import { Grid, Row, Col } from 'react-bootstrap/lib';
 import Helmet from 'react-helmet';
 import DevTools from './components/DevTools';
 import Header from './components/Header/Header';
@@ -30,35 +31,49 @@ export class App extends Component {
 
   render() {
     return (
-      <div>
+      <Grid>
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
-        <div>
-          <Helmet
-            title="Home - Central Realty"
-            titleTemplate="%s - Central Realty"
-            meta={[
-              { charset: 'utf-8' },
-              {
-                'http-equiv': 'X-UA-Compatible',
-                content: 'IE=edge',
-              },
-              {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1',
-              },
-            ]}
-          />
+        <Helmet
+          title="Home - Central Realty"
+          titleTemplate="%s - Central Realty"
+          meta={[
+            { charset: 'utf-8' },
+            {
+              'http-equiv': 'X-UA-Compatible',
+              content: 'IE=edge',
+            },
+            {
+              name: 'viewport',
+              content: 'width=device-width, initial-scale=1',
+            },
+          ]}
+          link={[
+            {"rel": "stylesheet", "href": "https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css"},
+          ]}
+          script={[
+            {"src": "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js",
+             "type": "text/javascript",
+             "integrity": "sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa",
+             "crossorigin": "anonymous"}
+          ]}
+        />
+
+      <Row>
           <Header
             switchLanguage={lang => this.props.dispatch(switchLanguage(lang))}
             intl={this.props.intl}
             toggleAddPost={this.toggleAddPostSection}
           />
+      </Row>
+      <Row>
           <div className={styles.container}>
             {this.props.children}
           </div>
+        </Row>
+        <Row>
           <Footer />
-        </div>
-      </div>
+        </Row>
+      </Grid>
     );
   }
 }
